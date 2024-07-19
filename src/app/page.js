4d2642c04a79d82dfde76422afeab00acadfc83e
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-19 09:53:10 +0700

    navigate using Link component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col gap-4 p-24">
       <h1>Next JS 13 Crash Course</h1>
+      <Link href={"/products"}>Go to products page</Link>
     </main>
   );
 }
