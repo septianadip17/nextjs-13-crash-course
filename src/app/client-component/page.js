905bd6ebfb15ba3a8d392e66cedbea7a357b5b7e
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-19 09:32:54 +0700

    server and client component

---
+++ src/app/client-component/page.js
@@ -0,0 +1,11 @@
+"use client"
+import { useState } from "react"
+
+export default function ClientComponent(){
+  const [toggle, setToggle] = useState(false)
+  
+  
+  return (
+    <h1>This is client component</h1>
+  )
+}
---
+++ src/app/server-component/page.js
@@ -0,0 +1,6 @@
+
+export default function ServerComponent(){
+  return (
+    <h1>This is a server component</h1>
+  )
+}
