3a1a1604188729991b41fb624f432ee7dcd7e8d6
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-19 10:56:32 +0700

    fetching data in dynamic routes

--- src/app/products/[productdetails]/page.js
+++ src/app/products/[productdetails]/page.js
@@ -1,3 +1,19 @@
-export default function ProductDetails() {
-  return <h1>This is a dynamic route for Product Details</h1>;
+async function fetchProductDetails(id) {
+  const response = await fetch(`https://dummyjson.com/products/${id}`, {
+    method: "GET",
+  });
+
+  const result = await response.json();
+  return result;
+}
+
+export default async function ProductDetails({ params }) {
+  const { productdetails } = params;
+  const getCurrentProductDetails = await fetchProductDetails(productdetails);
+  console.log(getCurrentProductDetails);
+  return <div>
+    <h1>{getCurrentProductDetails?.title}</h1>
+    <p>{getCurrentProductDetails?.description}</p>
+    <p>{getCurrentProductDetails?.price}</p>
+  </div>
 }
